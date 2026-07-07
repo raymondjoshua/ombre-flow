fc256f7a647c1588fb19fb0e409b137e5da097e7
Raymond <raymond@ombre-1.com>
2026-07-07 17:10:16 +0200

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // Add this temporary fix:
+    {
+      name: 'close-bundle',
+      closeBundle: () => {
+        process.exit(0);
+      }
+    }
+  ],
 })
